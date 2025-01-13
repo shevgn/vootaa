24f656508e16d8906d27752e5a4a2569891eb84b
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-01-13 15:45:10 +0200

    color mode switch

---
+++ frontend/components/color-mode-switch.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+
+const toggleColorMode = () => {
+  colorMode.preference = colorMode.preference === "dark" ? "light" : "dark";
+};
+
+const icons: Record<string, string> = {
+  dark: "ic:outline-dark-mode",
+  light: "ic:baseline-wb-sunny",
+} as const;
+
+console.log(colorMode.preference);
+</script>
+
+<template>
+  <button @click="toggleColorMode">
+    <UIcon :name="icons[colorMode.preference]" class="w-6 h-6" />
+  </button>
+</template>
