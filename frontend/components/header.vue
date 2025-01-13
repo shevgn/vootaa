d6bc031b22397d8fe6e9816ef25e68f47b908d22
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-01-13 14:45:27 +0200

    ui components and tailwindcss

--- frontend/components/header.vue
+++ frontend/components/header.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts"></script>
+
 <template>
-  <header>
-    <h1>Vootaa</h1>
+  <header class="flex flex-row p-4 items-center">
+    <UIcon name="si:add-circle-fill" class="w-8 h-8" />
+    <h1 class="text-xl ml-4">Header</h1>
   </header>
 </template>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import { resolve } from "path";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/ui']
-})
+  modules: ["@nuxt/ui", "@nuxtjs/tailwindcss"],
+  alias: {
+    "#tailwind-config": resolve("./tailwind.config"),
+  },
+});
