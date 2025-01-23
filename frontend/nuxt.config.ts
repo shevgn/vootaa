dea42cb3b5f3f0071bd967dc7b967318eea763f4
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-01-23 16:02:29 +0200

    new compatibility

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,7 +3,7 @@
 import { resolve } from "path";
 
 export default defineNuxtConfig({
-    compatibilityDate: "2024-11-01",
+    compatibilityDate: "2025-01-22",
     devtools: { enabled: true },
     modules: [
         "@nuxt/ui",
