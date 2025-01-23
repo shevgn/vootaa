9cfc94de0c97c8f818b771ee737083b0cbbc1f40
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-01-23 21:25:38 +0200

    clean up

--- frontend/components/chainweb.vue
+++ frontend/components/chainweb.vue
@@ -5,7 +5,7 @@
     class="relative hidden h-full w-full max-w-screen-sm rounded-md border border-custom-dark p-4 dark:border-custom-cyan lg:block"
   >
     <div
-      class="absolute left-4 right-4 top-4 mb-4 flex flex-row justify-between text-lg font-semibold"
+      class="absolute left-4 right-4 top-4 mb-4 flex flex-row justify-between font-semibold"
     >
       <span class="text-custom-dark dark:text-custom-cyan">CHAINWEB</span>
       <span class="text-custom-dark dark:text-custom-cyan">
