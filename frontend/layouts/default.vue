79668c75a8431341a41a13a285d28c883fd03af6
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-01-23 15:00:31 +0200

    chainweb in layout

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -2,7 +2,10 @@
 <template>
   <div class="h-full w-full p-4">
     <Header />
-    <slot />
+    <main class="my-8 flex h-full w-full flex-row">
+      <Chainweb />
+      <slot />
+    </main>
     <Footer />
   </div>
 </template>
--- frontend/pages/swap.vue
+++ frontend/pages/swap.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="my-8 flex h-full w-full flex-row">
-    <Chainweb />
-    <Swap />
-  </div>
+  <Swap />
 </template>
