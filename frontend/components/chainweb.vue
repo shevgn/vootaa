d040bc936076ba655dc246e0698980948fde0485
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-01-29 17:31:40 +0200

    chain select added

--- frontend/components/chainweb.vue
+++ frontend/components/chainweb.vue
@@ -1,20 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const hoveredNode = ref<number | null>(null);
+const selectedNode = ref<number | null>(null);
+
+const handleNodeHover = (nodeId: number | null) => {
+  hoveredNode.value = nodeId;
+};
+
+const handleNodeClick = (nodeId: number) => {
+  selectedNode.value = nodeId;
+};
+</script>
 
 <template>
   <div
     class="relative hidden h-full w-full max-w-screen-sm rounded-md border border-custom-dark p-4 dark:border-custom-cyan lg:block"
   >
     <div
-      class="absolute left-4 right-4 top-4 mb-4 flex flex-row justify-between font-semibold"
+      class="absolute left-4 right-4 top-4 -z-10 mb-4 flex flex-row justify-between font-semibold"
     >
       <span class="text-custom-dark dark:text-custom-cyan">CHAINWEB</span>
       <span class="text-custom-dark dark:text-custom-cyan">
         CHAIN ID:
         <span class="text-red-400">
-          {{ "None" }}
+          {{ selectedNode || "None" }}
         </span>
       </span>
     </div>
-    <SvgChainweb />
+    <SvgChainweb
+      :handle-node-hover="handleNodeHover"
+      :handle-node-click="handleNodeClick"
+    />
   </div>
 </template>
