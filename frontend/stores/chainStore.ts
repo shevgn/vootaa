12c9f0694ac92fa099e4a595b5530de5c0ca50e6
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-01-29 17:32:04 +0200

    test store for chains

---
+++ frontend/stores/chainStore.ts
@@ -0,0 +1,10 @@
+import { defineStore } from "pinia";
+
+export default useChainStore = defineStore("chain", () => {
+  const selectedNode = ref<number | null>(null);
+  const setSelectedNode = (id: number | null) => {
+    selectedNode.value = id;
+  };
+
+  return { selectedNode, setSelectedNode };
+});
