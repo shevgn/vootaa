4da514c812126015f6c5658605099306a4a5d5ef
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-01-29 17:31:30 +0200

    custom underline deleted

--- frontend/components/header.vue
+++ frontend/components/header.vue
@@ -7,95 +7,10 @@ const path = computed(() => {
   return route.path;
 });
 
-const underlineStylesProps = ref({
-  left: 0,
-  width: 0,
-});
-
-const underlineStyles = computed(() => {
-  return {
-    left: underlineStylesProps.value.left + "px",
-    width: underlineStylesProps.value.width + "px",
-  };
-});
-
-const updateUnderline = async (): Promise<void> => {
-  const activeLink = document.querySelector(
-    `a[href="${path.value}"]`
-  ) as HTMLElement;
-
-  if (!activeLink) {
-    return;
-  }
-
-  const rect = activeLink.getBoundingClientRect();
-  const nav = document.querySelector("nav")?.getBoundingClientRect();
-
-  if (!nav) {
-    return;
-  }
-
-  while (
-    underlineStylesProps.value.left > rect.left - nav.left ||
-    underlineStylesProps.value.width < rect.width ||
-    underlineStylesProps.value.width > rect.width
-  ) {
-    if (underlineStylesProps.value.left > rect.left - nav.left) {
-      underlineStylesProps.value.left -= 2;
-    }
-    if (underlineStylesProps.value.width > rect.width) {
-      if (Math.abs(underlineStylesProps.value.width - rect.width) <= 2) {
-        underlineStylesProps.value.width = rect.width;
-      } else {
-        underlineStylesProps.value.width -= 1;
-      }
-    } else if (underlineStylesProps.value.width < rect.width) {
-      if (Math.abs(underlineStylesProps.value.width - rect.width) <= 2) {
-        underlineStylesProps.value.width = rect.width;
-      } else {
-        underlineStylesProps.value.width += 1;
-      }
-    }
-    await new Promise((resolve) => setTimeout(resolve, 2));
-  }
-  while (
-    underlineStylesProps.value.left < rect.left - nav.left ||
-    underlineStylesProps.value.width < rect.width ||
-    underlineStylesProps.value.width > rect.width
-  ) {
-    if (underlineStylesProps.value.left < rect.left - nav.left) {
-      underlineStylesProps.value.left += 2;
-    }
-    if (underlineStylesProps.value.width > rect.width) {
-      if (Math.abs(underlineStylesProps.value.width - rect.width) <= 2) {
-        underlineStylesProps.value.width = rect.width;
-      } else {
-        underlineStylesProps.value.width -= 1;
-      }
-    } else if (underlineStylesProps.value.width < rect.width) {
-      if (Math.abs(underlineStylesProps.value.width - rect.width) <= 2) {
-        underlineStylesProps.value.width = rect.width;
-      } else {
-        underlineStylesProps.value.width += 1;
-      }
-    }
-    await new Promise((resolve) => setTimeout(resolve, 2));
-  }
-};
-
-watch(
-  () => path.value,
-  () => {
-    updateUnderline();
-  }
-);
-
 onMounted(() => {
-  updateUnderline();
   setTimeout(() => {
     mounted.value = true;
   }, 100);
-  console.log("mounted");
 });
 </script>
 
@@ -104,21 +19,10 @@ onMounted(() => {
     <h1 class="sr-only text-lg text-custom-dark dark:text-custom-cyan">
       Vootaa
     </h1>
-    <svg
-      class="mr-1 h-12 w-12"
-      viewBox="0 0 500 500"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-    >
+    <svg class="mr-1 h-12 w-12" viewBox="0 0 500 500" version="1.1" xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink">
       <title>logo</title>
-      <g
-        id="vootaa"
-        stroke="none"
-        stroke-width="1"
-        fill="none"
-        fill-rule="evenodd"
-      >
+      <g id="vootaa" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
         <g id="logo" stroke="">
           <!--
           <g id="bg-group">
@@ -127,70 +31,53 @@ onMounted(() => {
           </g>
 -->
           <g id="logo-group" transform="translate(80, 42)" stroke-width="10">
-            <path
-              id="path"
-              class="fill-custom-dark dark:fill-custom-cyan"
+            <path id="path" class="fill-custom-dark dark:fill-custom-cyan"
               d="M165.38866,346.818251 L165.38866,360.331212 C165.38866,387.945449 143.002897,410.331212 115.38866,410.331212 C87.7744222,410.331212 65.3886597,387.945449 65.3886597,360.331212 L65.3886597,60.3312117 C65.3886597,32.7169742 87.7744222,10.3312117 115.38866,10.3312117 C143.002897,10.3312117 165.38866,32.7169742 165.38866,60.3312117 L165.38866,256.990445 L165.38866,256.990445"
-              transform="translate(115.3887, 210.3312) rotate(-20) translate(-115.3887, -210.3312)"
-            />
-            <path
-              id="circle"
-              class="fill-custom-green stroke-white dark:stroke-custom-black"
+              transform="translate(115.3887, 210.3312) rotate(-20) translate(-115.3887, -210.3312)" />
+            <path id="circle" class="fill-custom-green stroke-white dark:stroke-custom-black"
               d="M179.096098,238.800919 C189.526707,233.94026 201.812819,233.056151 213.482106,237.300618 C225.023732,241.49865 233.800692,249.92764 238.696589,260.197381 C243.597095,270.476788 244.609786,282.600334 240.613702,294.169629 L240.313496,295.012583 C236.064601,306.664866 227.493267,315.494813 217.068709,320.352652 C206.6381,325.21331 194.351989,326.097419 182.682701,321.852953 C171.141075,317.65492 162.364116,309.225931 157.468218,298.95619 C152.567713,288.676783 151.555021,276.553237 155.551106,264.983941 L155.851311,264.140987 C160.100207,252.488705 168.67154,243.658758 179.096098,238.800919 Z"
-              transform="translate(198.0824, 279.5768) rotate(20) translate(-198.0824, -279.5768)"
-            />
-            <rect
-              id="box"
-              class="fill-custom-dark dark:fill-custom-cyan"
-              transform="translate(259.5939, 111.0703) rotate(20) translate(-259.5939, -111.0703)"
-              x="214.593909"
-              y="16.0702692"
-              width="90"
-              height="190"
-              rx="45"
-            />
+              transform="translate(198.0824, 279.5768) rotate(20) translate(-198.0824, -279.5768)" />
+            <rect id="box" class="fill-custom-dark dark:fill-custom-cyan"
+              transform="translate(259.5939, 111.0703) rotate(20) translate(-259.5939, -111.0703)" x="214.593909"
+              y="16.0702692" width="90" height="190" rx="45" />
           </g>
         </g>
       </g>
     </svg>
     <div class="flex w-full justify-between">
       <nav class="relative flex items-center">
-        <ul
-          class="flex flex-row items-center space-x-8 text-custom-dark dark:text-custom-cyan"
-        >
-          <li>
-            <NuxtLink to="/" class="mr-4 text-xl">Vootaa</NuxtLink>
+        <ul class="flex flex-row items-center space-x-8 text-custom-dark dark:text-custom-cyan">
+          <li class="mr-4">
+            <NuxtLink to="/" class="text-xl">Vootaa</NuxtLink>
           </li>
-          <li>
+          <li class="mr-4 border-b-2 border-custom-dark transition dark:border-custom-cyan" :class="path === '/pool'
+              ? 'border-opacity-100 dark:border-opacity-100'
+              : 'border-opacity-0 dark:border-opacity-0'
+            ">
             <NuxtLink to="/pool" class="">Pool</NuxtLink>
           </li>
-          <li>
+          <li class="mr-4 border-b-2 border-custom-dark transition dark:border-custom-cyan" :class="path === '/liquidity'
+              ? 'border-opacity-100 dark:border-opacity-100'
+              : 'border-opacity-0 dark:border-opacity-0'
+            ">
             <NuxtLink to="/liquidity" class=""> Liquidity </NuxtLink>
           </li>
-          <li>
+          <li class="mr-4 border-b-2 border-custom-dark transition dark:border-custom-cyan" :class="path === '/swap'
+              ? 'border-opacity-100 dark:border-opacity-100'
+              : 'border-opacity-0 dark:border-opacity-0'
+            ">
             <NuxtLink to="/swap" class="">Swap</NuxtLink>
           </li>
         </ul>
-        <div
-          :style="underlineStyles"
-          class="absolute bottom-0 h-0.5 bg-custom-dark dark:bg-custom-cyan"
-        />
       </nav>
       <div class="flex items-center space-x-4">
         <form action="" class="h-full">
-          <input
-            type="text"
-            placeholder="input your K:account"
-            class="h-full w-64 rounded-lg bg-custom-gray px-4 py-1 text-custom-dark placeholder:text-custom-dark placeholder-shown:text-sm focus-visible:outline-custom-green dark:bg-custom-dark dark:text-custom-cyan dark:placeholder:text-custom-cyan dark:focus-visible:outline-custom-green"
-          />
+          <input type="text" placeholder="input your K:account"
+            class="h-full w-64 rounded-lg bg-custom-gray px-4 py-1 text-custom-dark placeholder:text-custom-dark placeholder-shown:text-sm focus-visible:outline-custom-green dark:bg-custom-dark dark:text-custom-cyan dark:placeholder:text-custom-cyan dark:focus-visible:outline-custom-green" />
         </form>
         <button
-          class="rounded-lg border border-custom-dark p-1 px-2 hover:bg-gray-100 dark:border-custom-cyan dark:hover:bg-custom-dark"
-        >
-          <span
-            class="text-sm text-custom-dark dark:text-custom-cyan lg:text-base"
-            >Connect to wallet</span
-          >
+          class="rounded-lg border border-custom-dark p-1 px-2 hover:bg-gray-100 dark:border-custom-cyan dark:hover:bg-custom-dark">
+          <span class="text-sm text-custom-dark dark:text-custom-cyan lg:text-base">Connect to wallet</span>
         </button>
         <USkeleton v-if="!mounted" class="h-6 w-6 rounded-full" />
         <ClientOnly v-else>
