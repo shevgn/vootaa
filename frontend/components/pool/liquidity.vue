d4da130b590a96845d31f6b99cfeace3c22b5305
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-02-14 20:00:34 +0200

    formatted text for pools accordion

---
+++ frontend/components/pool/liquidity.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+  chain?: number;
+  token1: {
+    value?: number;
+    name: string;
+  };
+  token2?: {
+    value: number;
+  };
+}>();
+</script>
+<template>
+  <div class="flex items-center justify-around py-1">
+    <span>
+      Liquidity
+      <sub v-if="!chain"> (Chain 0 - 19) </sub>
+      <sub v-else>(Chain #{{ chain }})</sub>
+    </span>
+    <span v-if="!token1.value">12,345,566.77 ${{ token1.name }}</span>
+    <span v-else>{{ token1.value }} {{ token1.name }}</span>
+    <span>-</span>
+    <span v-if="!token2">1,550,603.18 $VOOTAA</span>
+    <span v-else>{{ token2.value }} VOOTAA</span>
+  </div>
+</template>
