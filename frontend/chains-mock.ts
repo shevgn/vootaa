88ee86c1e95ac1b643d74596b388f43c8bc8579a
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-02-14 20:04:12 +0200

    test chains

---
+++ frontend/chains-mock.ts
@@ -0,0 +1,23 @@
+type Chain = {
+    id: number;
+    liquidity: number;
+    price: number;
+};
+
+export const chains: Chain[] = [
+    {
+        id: 1,
+        liquidity: 100,
+        price: 0.1211,
+    },
+    {
+        id: 2,
+        liquidity: 200,
+        price: 0.1111,
+    },
+    {
+        id: 3,
+        liquidity: 300,
+        price: 0.1212,
+    },
+];
