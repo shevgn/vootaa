8b9c6b3e51f98c5357fd8d09d30f1106b85cf6db
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-02-16 18:20:01 +0200

    fix: dollar sign added to token

--- frontend/components/pool/liquidity.vue
+++ frontend/components/pool/liquidity.vue
@@ -18,9 +18,9 @@ defineProps<{
       <sub v-else>(Chain #{{ chain }})</sub>
     </span>
     <span v-if="!token1.value">12,345,566.77 ${{ token1.name }}</span>
-    <span v-else>{{ token1.value }} {{ token1.name }}</span>
+    <span v-else>{{ token1.value }} ${{ token1.name }}</span>
     <span>-</span>
     <span v-if="!token2">1,550,603.18 $VOOTAA</span>
-    <span v-else>{{ token2.value }} VOOTAA</span>
+    <span v-else>{{ token2.value }} $VOOTAA</span>
   </div>
 </template>
