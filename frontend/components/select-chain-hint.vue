096227137b3a37027d05eca3e5379cd118a01ea9
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-03-02 15:40:22 +0200

    hint for selecting chainweb node

---
+++ frontend/components/select-chain-hint.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="flex h-20 items-center justify-center text-red-500">
+    <UIcon name="ic:baseline-arrow-back" class="mr-4 h-8 w-8" />
+    <p>Click The Chain ID Number In The Left ChainWeb Panel</p>
+  </div>
+</template>
