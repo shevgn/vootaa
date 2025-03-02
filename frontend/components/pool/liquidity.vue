3e82a8d3b2a035e532d039a153abcfc080a0f241
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-03-02 15:39:48 +0200

    token type & proper undefined check

--- frontend/components/pool/liquidity.vue
+++ frontend/components/pool/liquidity.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import type { Token } from "~/types/tokens";
+
 defineProps<{
   title?: string;
   chain?: number;
   token1: {
     value?: number;
-    name: string;
+    name: Token;
   };
   token2?: {
     value: number;
@@ -15,13 +17,15 @@ defineProps<{
   <div class="flex items-center justify-around py-1">
     <span>
       {{ title ? title : "Liquidity" }}
-      <sub v-if="!chain"> (Chain 0 - 19) </sub>
+      <sub v-if="chain === undefined"> (Chain 0 - 19) </sub>
       <sub v-else>(Chain #{{ chain }})</sub>
     </span>
-    <span v-if="!token1.value">12,345,566.77 ${{ token1.name }}</span>
-    <span v-else>{{ token1.value }} ${{ token1.name }}</span>
+    <span v-if="token1.value === undefined">
+      12,345,566.77 ${{ token1.name }}
+    </span>
+    <span v-else>{{ token1.value.toLocaleString() }} ${{ token1.name }}</span>
     <span>-</span>
     <span v-if="!token2">1,550,603.18 $VOOTAA</span>
-    <span v-else>{{ token2.value }} $VOOTAA</span>
+    <span v-else>{{ token2.value.toLocaleString() }} $VOOTAA</span>
   </div>
 </template>
