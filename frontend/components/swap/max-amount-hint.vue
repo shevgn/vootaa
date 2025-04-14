e5aa7d678907d9022d68d2e92a6114fc58dc04bc
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-04-14 19:52:14 +0300

    max & min amount hint components

---
+++ frontend/components/swap/max-amount-hint.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const maxHintShown = ref(false);
+
+const showMaxHint = (): void => {
+  maxHintShown.value = !maxHintShown.value;
+};
+</script>
+
+<template>
+  <button
+    type="button"
+    class="absolute right-1 top-0 flex size-5 items-center justify-center rounded-full bg-custom-dark dark:bg-custom-cyan"
+    @click="showMaxHint"
+  >
+    <UIcon
+      name="ic:baseline-question-mark"
+      class="h-3 w-3 text-custom-cyan dark:bg-custom-dark"
+    />
+  </button>
+  <p
+    v-if="maxHintShown"
+    class="absolute -top-1 right-8 text-xs font-medium text-red-500"
+  >
+    Limit to 1% of pool size
+  </p>
+</template>
---
+++ frontend/components/swap/min-amount-hint.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+defineProps<{
+  minAmount: number;
+}>();
+const minHintShown = ref(false);
+
+const showMinHint = (): void => {
+  minHintShown.value = !minHintShown.value;
+};
+</script>
+<template>
+  <button
+    type="button"
+    class="absolute left-1 top-0 flex size-5 items-center justify-center rounded-full bg-custom-dark dark:bg-custom-cyan"
+    @click="showMinHint"
+  >
+    <UIcon
+      name="ic:baseline-question-mark"
+      class="h-3 w-3 text-custom-cyan dark:bg-custom-dark"
+    />
+  </button>
+  <p
+    v-if="minHintShown"
+    class="absolute -top-1 left-8 text-xs font-medium text-red-500"
+  >
+    Limit the minimum amout to {{ minAmount }}
+  </p>
+</template>
