16feda2b568073e32f3468b07fd5c958f343f4f1
Левченко Артем Андрійович <ipz233_laa@student.ztu.edu.ua>
2025-04-14 19:45:46 +0300

    result amount of swap operaiont as component

---
+++ frontend/components/swap/result-amount.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+defineProps<{
+  originalAmount: number;
+  amountToReceive: number;
+  tokens: {
+    tokenToSell: string;
+    tokenToBuy: string;
+  };
+}>();
+
+type FormattedAmount = {
+  wholePart: string;
+  pennyPart: string;
+};
+
+const formatResultAmount = (amount: number): FormattedAmount => {
+  const splitted = amount.toFixed(4).toString().split(".");
+
+  return {
+    wholePart: splitted[0],
+    pennyPart: splitted[1],
+  };
+};
+</script>
+
+<template>
+  <div class="flex w-full items-center justify-evenly p-2">
+    <div>
+      <span class="text-3xl">
+        {{ formatResultAmount(originalAmount).wholePart }}.
+      </span>
+      <span class="pr-4">
+        {{ formatResultAmount(originalAmount).pennyPart }}
+      </span>
+      <span>${{ tokens.tokenToSell }}</span>
+    </div>
+    <UIcon
+      name="ic:baseline-keyboard-double-arrow-right"
+      class="h-8 w-8 bg-custom-dark dark:bg-custom-cyan"
+    />
+    <div>
+      <span class="text-3xl">
+        {{ formatResultAmount(amountToReceive).wholePart }}.
+      </span>
+      <span class="pr-4">
+        {{ formatResultAmount(amountToReceive).pennyPart }}
+      </span>
+      <span>${{ tokens.tokenToBuy }}</span>
+    </div>
+  </div>
+</template>
